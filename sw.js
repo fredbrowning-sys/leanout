ccc25c7360f691a491ddd36a05f9cc68b19bd925
fredbrowning-sys <freddie.t.browning@gmail.com>
2026-09-07 19:25:41 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "delta-v9";
+const CACHE = "delta-v10";
 const ASSETS = ["./", "./index.html", "./icon-180.png", "./icon-512.png", "./manifest.json"];
 
 self.addEventListener("install", e => {
